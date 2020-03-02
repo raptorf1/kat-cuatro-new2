49afbb46c28f653e9274e7c30dda142e098d8960
raptorf1 <raptor_f1@hotmail.com>
2020-03-02 11:07:05 +0200

    Error handling for Login component

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
-import { Form, Button } from 'semantic-ui-react'
+import { Form, Button, Message } from 'semantic-ui-react'
 
 const Login = (props) => {
 
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [errorDisplay, setErrorDisplay] = useState(false)
+  const [errors, setErrors] = useState([])
 
   const correctUsername = 'test'
   const correctPassword = 'test'
@@ -12,34 +14,44 @@ const Login = (props) => {
   const login = () => {
     if (username === correctUsername && password === correctPassword) {
       props.history.push('/calculator')
+    } else {
+      setErrors(['Invalid login credentials'])
+      setErrorDisplay(true)
     }
   }
 
   return (
-    <Form>
-      <Form.Field>
-        <input
-          placeholder='Username'
-          required={true}
-          onChange={(e) => setUsername(e.target.value)}
-          onKeyPress={e => { e.key === 'Enter' && login() }}
-        />
-      </Form.Field>
-      <Form.Field>
-        <input
-          placeholder='Password'
-          type='password'
-          required={true}
-          onChange={(e) => setPassword(e.target.value)}
-          onKeyPress={e => { e.key === 'Enter' && login() }}
-        />
-      </Form.Field>
-      <Button
-        onClick={() => login()}
-      >
-        Login
+    <>
+      <Form>
+        <Form.Field>
+          <input
+            placeholder='Username'
+            required={true}
+            onChange={(e) => setUsername(e.target.value)}
+            onKeyPress={e => { e.key === 'Enter' && login() }}
+          />
+        </Form.Field>
+        <Form.Field>
+          <input
+            placeholder='Password'
+            type='password'
+            required={true}
+            onChange={(e) => setPassword(e.target.value)}
+            onKeyPress={e => { e.key === 'Enter' && login() }}
+          />
+        </Form.Field>
+        <Button
+          onClick={() => login()}
+        >
+          Login
       </Button>
-    </Form>
+      </Form>
+      {errorDisplay &&
+        <Message negative style={{ 'textAlign': 'center' }} >
+          {errors}
+        </Message>
+      }
+    </>
   )
 }
 
