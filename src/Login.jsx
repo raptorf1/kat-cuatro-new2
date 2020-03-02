5bb7e4440e61daea540ccb577c0e91e1e28da19d
raptorf1 <raptor_f1@hotmail.com>
2020-03-02 10:47:19 +0200

    Sets up login form with appropriate options in every field

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import { List } from 'semantic-ui-react'
+import React, { useState } from 'react'
+import { Form, Button } from 'semantic-ui-react'
 
 const Login = () => {
 
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
-    'login yay'
+    <Form>
+      <Form.Field>
+        <input
+          placeholder='Username'
+          required={true}
+          onChange={(e) => setUsername(e.target.value)}
+        //  onKeyPress={e => { e.key === 'Enter' && logInUser() }}
+        />
+      </Form.Field>
+      <Form.Field>
+        <input
+          placeholder='Password'
+          type='password'
+          required={true}
+          onChange={(e) => setPassword(e.target.value)}
+          //  onKeyPress={e => { e.key === 'Enter' && logInUser() }}
+        />
+      </Form.Field>
+      <Button>Login</Button>
+    </Form>
   )
 }
 
