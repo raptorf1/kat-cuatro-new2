724e7805fcd7288ff805725d70eba5b2a854f059
raptorf1 <raptor_f1@hotmail.com>
2020-03-03 18:16:16 +0200

    Adds radio button to the Calculator form and changes state accordingly

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from 'react'
-import { Form, Button } from 'semantic-ui-react'
+import { Form, Button, Radio } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 
 const Calculator = (props) => {
 
   const [height, setHeight] = useState(1)
   const [width, setWidth] = useState(1)
+  const [type, setType] = useState('')
 
   useEffect(() => { props.userIn === false && props.history.push('/') })
 
   return (
     <Form
-      style={{ 'maxWidth': 'fit-content', 'textAlignLast': 'center', 'paddingTop': '1rem', 'paddingLeft': '5rem' }}
+    //    style={{ 'maxWidth': 'fit-content', 'textAlignLast': 'center', 'paddingTop': '1rem', 'paddingLeft': '5rem' }}
     >
       <Form.Field>
         <label>Ύψος σε μέτρα</label>
@@ -37,6 +38,27 @@ const Calculator = (props) => {
           required={true}
           onChange={(e) => setWidth(e.target.value)}
         //  onKeyPress={e => { e.key === 'Enter' && login() }}
+        />
+      </Form.Field>
+      <Form.Field>
+        <b>Τύπος Κουφώματος</b>
+      </Form.Field>
+      <Form.Field>
+        <Radio
+          label='Ίσιο'
+          name='radioGroup'
+          value='isio'
+          checked={type === 'isio'}
+          onChange={() => setType('isio')}
+        />
+      </Form.Field>
+      <Form.Field>
+        <Radio
+          label='Πομπέ'
+          name='radioGroup'
+          value='pompe'
+          checked={type === 'pompe'}
+          onChange={() => setType('pompe')}
         />
       </Form.Field>
       <Button
