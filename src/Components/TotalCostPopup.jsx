72658f21eef575612bc80e98974ce7a8c5e4015c
raptorf1 <raptor_f1@hotmail.com>
2020-03-05 18:16:12 +0200

    Basic styling of buttons in TotalCostPopup component

--- src/Components/TotalCostPopup.jsx
+++ src/Components/TotalCostPopup.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
+import { Button, Icon, Label } from 'semantic-ui-react'
 
 const TotalCostPopupOpen = (props) => {
 
   return (
-    <p>
-      Συνολικό κόστος: ${props.cost} €
-      Προτεινόμενη τιμή πώλησης: ${(props.cost * 1.15).toFixed(2)} €
-    </p>
+    <div
+      style={{ 'textAlign': 'center' }}>
+      <Button as='div' labelPosition='right'>
+        <Button color='teal' style={{ 'cursor': 'auto' }}>
+          <Icon name='warehouse' />
+          Κόστος
+        </Button>
+        <Label as='a' basic color='red' pointing='left' style={{ 'cursor': 'auto' }}>
+          {props.cost} €
+        </Label>
+      </Button>
+      <br></br>
+      <br></br>
+      <Button as='div' labelPosition='right'>
+        <Button color='brown' style={{ 'cursor': 'auto' }}>
+          <Icon name='user' />
+          Τιμή Πελάτη
+        </Button>
+        <Label as='a' basic color='green' pointing='left' style={{ 'cursor': 'auto' }}>
+          {(props.cost * 1.15).toFixed(2)} €
+        </Label>
+      </Button>
+    </div>
   )
 }
 
