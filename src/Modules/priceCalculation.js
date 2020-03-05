cccd28e5bc036ea354d8fd41e3fd51250f19b6c7
raptorf1 <raptor_f1@hotmail.com>
2020-03-05 19:54:49 +0200

    Code refactoring of function

--- src/Modules/priceCalculation.js
+++ src/Modules/priceCalculation.js
@@ -8,94 +8,62 @@ const priceCalculation = (height, width, type, dimensions, driver, axle) => {
   let akson = 0
   let totalCost
 
-  if (type === 'isio' && dimensions === '14x17') {
-    kouti = 13.06
-    plaina = 4.30
-    if (driver === true) {
-      odhg = 3.76
+  if (type === 'isio') {
+    if (dimensions === '14x17') {
+      kouti = 13.06
+      plaina = 4.30
+      driver === true && (odhg = 3.76)
+      axle === true && (akson = 2.50)
     }
-    if (axle === true) {
-      akson = 2.50
+    else if (dimensions === '14x19') {
+      kouti = 11.10
+      plaina = 4.16
+      driver === true && (odhg = 3.76)
+      axle === true && (akson = 2.50)
     }
-  }
-  if (type === 'isio' && dimensions === '14x19') {
-    kouti = 11.10
-    plaina = 4.16
-    if (driver === true) {
-      odhg = 3.76
+    else if (dimensions === '17x17') {
+      kouti = 14.70
+      plaina = 5.17
+      driver === true && (odhg = 3.76)
+      axle === true && (akson = 2.50)
     }
-    if (axle === true) {
-      akson = 2.50
+    else if (dimensions === '18x19') {
+      kouti = 14.70
+      plaina = 5.17
+      driver === true && (odhg = 3.76)
+      axle === true && (akson = 2.50)
     }
-  }
-  if (type === 'isio' && dimensions === '17x17') {
-    kouti = 14.70
-    plaina = 5.17
-    if (driver === true) {
-      odhg = 3.76
-    }
-    if (axle === true) {
-      akson = 2.50
-    }
-  }
-  if (type === 'isio' && dimensions === '18x19') {
-    kouti = 14.70
-    plaina = 5.17
-    if (driver === true) {
-      odhg = 3.76
-    }
-    if (axle === true) {
-      akson = 2.50
+    else if (dimensions === '20.6x20.6') {
+      kouti = 18.88
+      plaina = 7.30
+      driver === true && (odhg = 3.76)
+      axle === true && (akson = 2.50)
     }
   }
-  if (type === 'isio' && dimensions === '20.6x20.6') {
-    kouti = 18.88
-    plaina = 7.30
-    if (driver === true) {
-      odhg = 3.76
+  else if (type === 'pompe') {
+    if (dimensions === '14x19') {
+      kouti = 11.53
+      plaina = 4.20
+      driver === true && (odhg = 1.83)
+      axle === true && (akson = 1.30)
     }
-    if (axle === true) {
-      akson = 2.50
+    else if (dimensions === '15.5x18.5') {
+      kouti = 14.60
+      plaina = 4.80
+      driver === true && (odhg = 1.83)
+      axle === true && (akson = 1.30)
     }
-  }
-  if (type === 'pompe' && dimensions === '14x19') {
-    kouti = 11.53
-    plaina = 4.20
-    if (driver === true) {
-      odhg = 1.83
+    else if (dimensions === '18x19') {
+      kouti = 14.60
+      plaina = 4.80
+      driver === true && (odhg = 1.83)
+      axle === true && (akson = 1.30)
     }
-    if (axle === true) {
-      akson = 1.30
-    }
-  }
-  if (type === 'pompe' && dimensions === '15.5x18.5') {
-    kouti = 14.60
-    plaina = 4.80
-    if (driver === true) {
-      odhg = 1.83
-    }
-    if (axle === true) {
-      akson = 1.30
-    }
-  }
-  if (type === 'pompe' && dimensions === '18x19') {
-    kouti = 14.60
-    plaina = 4.80
-    if (driver === true) {
-      odhg = 1.83
-    }
-    if (axle === true) {
-      akson = 1.30
-    }
-  }
-  if (type === 'pompe' && dimensions === '20.6x20.6') {
-    kouti = 19.07
-    plaina = 7.80
-    if (driver === true) {
-      odhg = 1.83
-    }
-    if (axle === true) {
-      akson = 1.30
+    else if (dimensions === '20.6x20.6') {
+      kouti = 19.07
+      plaina = 7.80
+      driver === true && (odhg = 1.83)
+      axle === true && (akson = 1.30)
     }
   }
 
